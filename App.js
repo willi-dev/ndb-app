b967ac804a1d1588834af89de944fbdf94f6b02c
willi-dev <willi.ilmukomputer@gmail.com>
2018-05-08 18:05:36 +0700

    first learn

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
+  
+  constructor( props ){
+    super(props);
+    this.state = { text: '' };
+  }
+
+  _onPressButton(){
+    Alert.alert('You tapped the button');
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <TextInput 
+          style={styles.textInput}
+          placeholder="Type Here to Translate"
+          onChangeText={(text)=> this.setState({text})}
+        />
+        <Text style={styles.textPizza}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+
+        <Button
+          onPress={this._onPressButton}
+          title="Press Me"
+        />
       </View>
     );
   }
@@ -15,9 +35,14 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20
   },
+  textInput: {
+    height: 80,
+    fontSize: 22
+  },
+  textPizza: {
+    padding: 10, 
+    fontSize: 22
+  }
 });
